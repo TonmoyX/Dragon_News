8f2c3c331dfd12f5a266c0aa1d8c95c967dd6e05
TonmoyX <142309932+TonmoyX@users.noreply.github.com>
2026-05-02 19:14:52 +0600

    fetch updated

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,19 +1,25 @@
 export async function categoryData() {
   const res = await fetch(
-    "https://openapi.programming-hero.com/api/news/categories"
+    `https://openapi.programming-hero.com/api/news/categories`,{
+      cache:"no-store"
+    }
   );
   const data = await res.json();
   return data;
 }
 
 export const getCetagoryNewsData = async (category_id) => {
-  const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`)
+  const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`, {
+    cache:"no-store"
+  })
   const data = await res.json();
   return data.data;
 }
 
 export const newsDetails = async (news_id) =>{
-  const res = await fetch(`https://openapi.programming-hero.com/api/news/${news_id}`)
+  const res = await fetch(`https://openapi.programming-hero.com/api/news/${news_id}`, {
+    cache:"no-store"
+  })
   const data = await res.json();
   return data.data[0];
 }
